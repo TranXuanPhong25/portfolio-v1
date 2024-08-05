1f0cb012f35653b9dd4f5954b624c69c2078c5e4
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-08-05 22:09:04 +0700

    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import ProjectsView from "../views/ProjectsView.vue";
 const router = createRouter({
     //import.meta.env.BASE_URL as string
-    history: createWebHistory(import.meta.env.BASE_URL as string),
+    history: createWebHistory(),
     routes: [
         {
             path: "/",
