2930708ce0523e9efbbcd5736bcee07cd286d1a2
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-08-05 20:49:13 +0700

    chore: upload vue config
    Create main.yml
    
    test
    
    test
    
    retest
    
    try
    
    test2
    
    retsets
    
    tr
    
    changerouter
    
    a
    
    a
    
    chore: try gh-pages
    
    test
    
    test
    
    retest
    
    try
    
    test2
    
    retsets
    
    tr
    
    changerouter
    
    a
    
    a

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ProjectsView from "../views/ProjectsView.vue";
+<<<<<<< HEAD
+<<<<<<< HEAD
 import ContactView from "../views/ContactView.vue";
+=======
+>>>>>>> 8a61f05 (test)
 const router = createRouter({
     //import.meta.env.BASE_URL as string
     history: createWebHistory(),
+=======
+const router = createRouter({
+    //import.meta.env.BASE_URL as string
+<<<<<<< HEAD
+    history: createWebHistory(import.meta.env.BASE_URL as string),
+>>>>>>> 4af307d (changerouter)
+=======
+    history: createWebHistory(),
+>>>>>>> 1f0cb01 (a)
     routes: [
         {
             path: "/",
@@ -17,11 +30,17 @@ const router = createRouter({
             name: "projects",
             component: ProjectsView,
         },
+<<<<<<< HEAD
+<<<<<<< HEAD
         {
             path: "/contact",
             name: "contact",
             component: ContactView,
         },
+=======
+>>>>>>> 8a61f05 (test)
+=======
+>>>>>>> 4af307d (changerouter)
     ],
 });
 
---
+++ vue.config.ts
@@ -0,0 +1,6 @@
+module.exports = {
+    publicPath:
+        process.env.NODE_ENV === "production"
+            ? "/portfolio/" // note the trailing slash
+            : "/portfolio/",
+};
