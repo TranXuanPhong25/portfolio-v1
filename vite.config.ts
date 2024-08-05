a10a0a02f97a8f541a8723e6cbe13dd61e7b1030
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-08-05 22:02:50 +0700

    a

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/portfolio/",
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
