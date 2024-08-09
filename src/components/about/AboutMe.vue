9e32451ca8a1e1cee64b8fb7661f635c000615d4
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-08-09 08:17:02 +0700

    refactor: make about Component

---
+++ src/components/about/AboutMe.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+<template>
+   <section id="about" class="py-20 h-screen bg-gray-800">
+      <div class="container mx-auto text-center">
+         <h2 class="text-4xl font-semibold">About Me</h2>
+         <!-- <img src="path-to-your-profile-picture.jpg" alt="Profile Picture" class="mx-auto mt-8 rounded-full w-32 h-32"> -->
+         <p class="mt-4 max-w-2xl mx-auto">Một đoạn văn ngắn giới thiệu về bạn, kinh nghiệm, và mục tiêu nghề nghiệp.
+         </p>
+         <div class="mt-8 grid grid-cols-2 gap-4 max-w-md mx-auto">
+            <div class="bg-gray-700 p-4 rounded">HTML</div>
+            <div class="bg-gray-700 p-4 rounded">CSS</div>
+            <div class="bg-gray-700 p-4 rounded">JavaScript</div>
+            <div class="bg-gray-700 p-4 rounded">Vue.js</div>
+         </div>
+      </div>
+   </section>
+</template>
