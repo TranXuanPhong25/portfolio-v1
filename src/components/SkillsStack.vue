5eca21d3eb059007e3820fe0f14c0be7a96f6bb3
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-08-10 20:46:17 +0700

    feat: temp effect

--- src/components/SkillsStack.vue
+++ src/components/SkillsStack.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
+import FluidContainer from './skill/FluidContainer.vue';
 </script>
 <template>
-   <section id="skills" class="py-20 h-screen bg-backgroun">
+   <section id="skills" class="py-20 h-screen bg-backgroun relative">
       <div class="container mx-auto text-center">
          <h2 class="text-4xl font-semibold">Skills</h2>
          <div class="mt-8 grid grid-cols-2 gap-4 max-w-md mx-auto">
 
          </div>
       </div>
+      <FluidContainer />
    </section>
 </template>
