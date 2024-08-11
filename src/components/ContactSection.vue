031e1eb566f937a65979831d063679d6bf829ca9
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-08-11 20:46:45 +0700

    feat: mailto anchor and text animation

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+import MailTo from './contact/MailTo.vue';
+</script>
 <template>
-   <section id="contact"> </section>
+   <section id="contact" class="flex justify-center items-center w-screen h-[50vh] ">
+
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&to=tranxuanphong26525@gmail.com" target="_blank"
+         class="flex justify-center items-center">
+         <MailTo />
+      </a>
+   </section>
 </template>
---
+++ src/components/contact/MailTo.vue
@@ -0,0 +1,108 @@
+<template>
+   <button>
+
+      <span class="wrapper overflow-hidden ">
+         <span class="span-text-1">Mail</span>
+         <span class="span-text-2">me</span>
+         <span class="span-text-3">now</span>
+      </span>
+
+   </button>
+</template>
+<style scoped>
+button {
+   font-family: inherit;
+
+   display: -webkit-box;
+   display: -ms-flexbox;
+   display: flex;
+   -webkit-box-align: center;
+   -ms-flex-align: center;
+   justify-content: center;
+   align-items: center;
+   border: none;
+   border-radius: 4px;
+   overflow: hidden;
+   -webkit-transition: all 0.1s ease;
+   transition: all 0.2s;
+   cursor: pointer;
+}
+
+button:active {
+   -webkit-transform: scale(0.95);
+   -ms-transform: scale(0.95);
+   transform: scale(0.95);
+}
+
+
+.wrapper {
+   padding-top: 20vh;
+   padding-left: 5vh;
+   height: 40vh;
+   width: 110vw;
+   text-align: center;
+   /* background-color: #000; */
+   display: flex;
+   justify-content: center;
+
+
+}
+
+.wrapper span {
+
+   -webkit-text-stroke-width: 1.25px;
+   -webkit-text-stroke-color: #000;
+   font-size: 100px;
+   text-shadow: 0 0px #f3c623, 0 0px #f2aaaa;
+   transform: translate(0, 100%) rotate(4deg);
+   animation: jump 2s ease-in-out infinite;
+   display: inline-block;
+   font-family: "Titan One", cursive;
+   color: #fff;
+   margin: 0 10px;
+
+}
+
+.span-text-1 {
+   animation-delay: 0.15s;
+}
+
+.span-text-2 {
+   animation-delay: 0.3s;
+}
+
+.span-text-3 {
+   animation-delay: 0.45s;
+}
+
+@keyframes jump {
+   33% {
+      text-shadow: 0 60px #f37121, 0 150px #f2aaaa;
+   }
+
+   50% {
+      transform: translate(0, 0) rotate(-4deg);
+      text-shadow: 0 0px #8fc0a9, 0 0px #84a9ac;
+   }
+
+   66.67% {
+      text-shadow: 0 -60px #d54062, 0 -150px #8fc0a9;
+   }
+}
+
+
+
+@media screen and (max-height: 200px) {
+   .wrapper {
+      transform: scale(0.3, 0.3);
+   }
+}
+
+@media screen and (max-width: 576px) {
+   .wrapper {
+      span {
+         font-size: 60px;
+      }
+   }
+}
+</style>
