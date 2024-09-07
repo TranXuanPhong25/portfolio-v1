ea68c6215eff3986d8830bd5d97a3ef7ed6709dd
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-09-07 13:16:00 +0700

    fix: overflow footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -20,7 +20,7 @@ onMounted(() => {
 <template>
 
 
-   <footer class=" w-full relative">
+   <footer class=" w-full relative overflow-hidden">
       <div class="w-full z-10 " data-aos="fade-up">
          <svg width="100%" height="100%" id="svg" viewBox="0 0 1440 360" xmlns="http://www.w3.org/2000/svg"
             class="-translate-y-0">
