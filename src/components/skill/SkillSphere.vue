8857a4e2ed81c212312d1ec900fc0658025d203a
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-09-07 07:49:25 +0700

    feat: test new sphere style

--- src/components/skill/SkillSphere.vue
+++ src/components/skill/SkillSphere.vue
@@ -5,8 +5,8 @@
 <style>
 .icon-cloud {
    backdrop-filter: blur(10px) brightness(0.8);
-   width: 40vw;
-   height: 40vw;
+   width: 800px;
+   height: 800px;
    position: relative;
    display: flex;
    justify-content: center;
@@ -15,6 +15,23 @@
    z-index: 30;
    border-radius: 50%;
    cursor: grab;
+}
+
+@media screen and (max-width: 900px) {
+   .icon-cloud {
+      width: 90vw;
+      height: 90vw;
+   }
+
+}
+
+@media screen and (max-width: 768px) {
+   .icon-cloud {
+      border-radius: 20px;
+      width: 100vw;
+      height: 70vh;
+
+   }
 }
 </style>
 <script setup lang="ts">
