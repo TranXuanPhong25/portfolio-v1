849b6bcf4f943b8cc1c91d8409d2e2fb3d597c18
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-09-07 10:43:03 +0700

    style: responsive projectcard for moblie

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -36,23 +36,24 @@ onMounted(() => {
 <template>
    <section class="bg-second/70 flex-col items-center justify-center p-1 z-30 " id="projects" ref="project">
       <div v-for="project in projectsData" :key="project.title"
-         class="project-card-wrapper bg-background  w-[70vw] min-h-[90vh] overflow-hidden  my-[100px] mx-auto py-1 rounded-lg shadow-md shadow-black"
+         class="project-card-wrapper bg-background w-[calc(95vw-20px)] lg:w-[80vw] 2xl:w-[70vw] h-auto overflow-hidden  my-[100px] mx-auto py-1 rounded-lg shadow-md shadow-black"
          data-aos="flip-down">
          <div class="w-[99.5%] project-card bg-background mx-auto  rounded-lg  ">
-            <div class="project-card-text w-full p-10 " data-aos="fade-right">
-               <h1 class="text-8xl font-bold mb-1">{{ project.title }}</h1>
+            <div class="project-card-text w-full z-10 p-5 sm:p-10 " data-aos="fade-right">
+               <h1 class="text-4xl sm:text-6xl md:text-8xl font-bold mb-3">{{ project.title }}</h1>
                <p class="text-xl">{{ project.description }}</p>
             </div>
-            <div class="project-card-image w-full rounded-lg block group relative " data-aos="zoom-in-down">
+            <div class="project-card-image w-full rounded-lg block group relative overflow-hidden"
+               data-aos="zoom-in-down">
                <img :src="project.image" alt="project image"
                   class=" w-full group-hover:blur-[4px] group-hover:brightness-50 duration-300 ">
-               <div class="">
-                  <a :href="project.demo" target="_blank"
-                     class="border-2 border-main  text-2xl text-white bg-primary rounded-md p-2 m-2 group-hover:top-[25%] top-[90%] left-[50%] translate-x-[-50%] absolute  duration-300 hover:bg-main hover:text-white">Demo</a>
-                  <a :href="project.repo" target="_blank"
-                     class="border-2 border-main  text-2xl text-white bg-primary rounded-md p-2 m-2 group-hover:top-[calc(25%+55px)] top-[90%] left-[50%] translate-x-[-50%] absolute  duration-300 hover:bg-main hover:text-white">View
-                     source</a>
-               </div>
+
+               <a :href="project.demo" target="_blank"
+                  class="border-2 border-main  text-2xl text-white bg-primary rounded-md p-2 m-2 group-hover:top-[35%] top-[-50%] left-[50%] translate-x-[-50%] absolute  duration-300 hover:bg-main hover:text-white">Demo</a>
+               <a :href="project.repo" target="_blank"
+                  class="border-2 border-main  text-2xl text-white bg-primary rounded-md p-2 m-2 group-hover:top-[calc(35%+55px)] top-[-50%] left-[50%] translate-x-[-50%] absolute  duration-300 hover:bg-main hover:text-white">View
+                  source</a>
+
             </div>
          </div>
       </div>
